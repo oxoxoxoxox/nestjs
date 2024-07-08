8a053ffb8570f23b8094c25472c01f667b7eae61
oxoxoxoxox <dudwlsaos132@naver.com>
2024-07-09 02:06:53 +0900

    Initial commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,7 @@ import { AddService, AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  @Get('/hello/')
+  @Get('/welcome/')
   getHello(): string {
     return this.appService.getHello();
   }
